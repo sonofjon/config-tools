52e2f71df4ed198d150c02bacff45e16ec17558a
Andreas Jonsson <ajdev8@gmail.com>
2024-03-05 01:52:06 +0100

    Disable options and functions

--- .fzf.bash
+++ .fzf.bash
@@ -3,10 +3,10 @@
 ## Options
 
 # Completion trigger sequence
-export FZF_COMPLETION_TRIGGER='~~'
+# export FZF_COMPLETION_TRIGGER='~~'
 
 # Completion options
-export FZF_COMPLETION_OPTS='--border --info=inline'
+# export FZF_COMPLETION_OPTS='--border --info=inline'
 
 ## Functions
 
@@ -16,30 +16,30 @@ export FZF_COMPLETION_OPTS='--border --info=inline'
 # - The first argument to the function ($1) is the base path to start
 #   traversal
 # - See the source code (completion.{bash,zsh}) for the details
-_fzf_compgen_path() {
-  fd --hidden --follow --exclude ".git" . "$1"
-}
+# _fzf_compgen_path() {
+#   fd --hidden --follow --exclude ".git" . "$1"
+# }
 
 # Custom directory completion
 #
 # Use fd to generate the list for directory completion
-_fzf_compgen_dir() {
-  fd --type d --hidden --follow --exclude ".git" . "$1"
-}
+# _fzf_compgen_dir() {
+#   fd --type d --hidden --follow --exclude ".git" . "$1"
+# }
 
 # Custom command completion
 #
 # Advanced customization of fzf options via _fzf_comprun function
 # - The first argument to the function is the name of the command.
 # - You should make sure to pass the rest of the arguments to fzf.
-_fzf_comprun() {
-  local command=$1
-  shift
+# _fzf_comprun() {
+#   local command=$1
+#   shift
 
-  case "$command" in
-    cd)           fzf --preview 'tree -C {} | head -200'   "$@" ;;
-    export|unset) fzf --preview "eval 'echo \$'{}"         "$@" ;;
-    ssh)          fzf --preview 'dig {}'                   "$@" ;;
-    *)            fzf --preview 'bat -n --color=always {}' "$@" ;;
-  esac
-}
+#   case "$command" in
+#     cd)           fzf --preview 'tree -C {} | head -200'   "$@" ;;
+#     export|unset) fzf --preview "eval 'echo \$'{}"         "$@" ;;
+#     ssh)          fzf --preview 'dig {}'                   "$@" ;;
+#     *)            fzf --preview 'bat -n --color=always {}' "$@" ;;
+#   esac
+# }
