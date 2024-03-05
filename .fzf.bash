e7d107dc053696311f8788b8111cd049aff4f21d
Andreas Jonsson <ajdev8@gmail.com>
2024-03-05 02:01:44 +0100

    Add options for key bindings

--- .fzf.bash
+++ .fzf.bash
@@ -43,3 +43,23 @@
 #     *)            fzf --preview 'bat -n --color=always {}' "$@" ;;
 #   esac
 # }
+
+### Key bindings
+
+# Ctrl-R options
+export FZF_CTRL_R_OPTS="
+  --height=10
+  --preview 'echo {}' --preview-window up:3:hidden:wrap
+  --bind 'ctrl-/:toggle-preview'
+  --bind 'ctrl-y:execute-silent(echo -n {2..} | pbcopy)+abort'"
+
+# Ctrl-T options
+export FZF_CTRL_T_OPTS="
+  --height=10
+  --preview 'cat {}' --preview-window down:5:hidden:wrap
+  --bind 'ctrl-/:toggle-preview'"
+
+# Alt-C options
+export FZF_ALT_C_OPTS="
+  --height=10"
+
