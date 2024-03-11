e4986c78178f43711c647608b07676796ca43380
Andreas Jonsson <ajdev8@gmail.com>
2024-03-11 12:46:12 +0100

    Hide info

--- .fzf.bash
+++ .fzf.bash
@@ -3,6 +3,7 @@
 # Ctrl-R options
 export FZF_CTRL_R_OPTS="
   --height=10
+  --info=hidden
   --preview 'echo {}' --preview-window up:3:hidden:wrap
   --bind 'ctrl-/:toggle-preview'
   --bind 'ctrl-y:execute-silent(echo -n {2..} | pbcopy)+abort'"
@@ -10,12 +11,14 @@ export FZF_CTRL_R_OPTS="
 # Ctrl-T options
 export FZF_CTRL_T_OPTS="
   --height=10
+  --info=hidden
   --preview 'cat {}' --preview-window down:5:hidden:wrap
   --bind 'ctrl-/:toggle-preview'"
 
 # Alt-C options
 export FZF_ALT_C_OPTS="
-  --height=10"
+  --height=10
+  --info=hidden
 
 # Completion trigger sequence
 # export FZF_COMPLETION_TRIGGER='~~'
