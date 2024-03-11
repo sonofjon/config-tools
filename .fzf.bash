a78e6af5e5161339238de8a06658c3b5cfc4c7ac
Andreas Jonsson <ajdev8@gmail.com>
2024-03-11 12:49:35 +0100

    Preserve prompt

--- .fzf.bash
+++ .fzf.bash
@@ -5,6 +5,7 @@ export FZF_CTRL_R_OPTS="
   --height=10
   --info=hidden
   --layout=reverse
+  --prompt='$USER at $HOSTNAME in $(pwd | sed "s,/home/$USER,~,g")$ '
   --preview 'echo {}' --preview-window up:3:hidden:wrap
   --bind 'ctrl-/:toggle-preview'
   --bind 'ctrl-y:execute-silent(echo -n {2..} | pbcopy)+abort'"
